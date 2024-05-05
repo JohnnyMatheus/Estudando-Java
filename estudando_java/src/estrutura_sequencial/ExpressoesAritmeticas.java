f8d1d6ef7c79fabee120b1e1f8f73a45ffbf59bf
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-05-05 18:33:03 -0300

    Create ExpressoesAritmeticas.java

---
+++ estudando_java/src/estrutura_sequencial/ExpressoesAritmeticas.java
@@ -0,0 +1,27 @@
+package estrutura_sequencial;
+
+public class ExpressoesAritmeticas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Exemplos de expressões aritméticas
+		int a, b, c, d, e, f, g;
+		a = 2 * 6 / 3;
+		System.out.println("Resultado = " + a);
+		b = 3 + 2 * 4;
+		System.out.println("Resultado = " + b);
+		c = (3 + 2) * 4;
+		System.out.println("Resultado = " + c);
+		d = 60 / (3 + 2) * 4;
+		System.out.println("Resultado = " + d);
+		e = 60 / ((3 + 2) * 4);
+		System.out.println("Resultado = " + e);
+
+		// Exemplos com o operador mod
+		f = 14 % 3;
+		System.out.println("Resultado = " + f);
+		g = 19 % 5;
+		System.out.println("Resultado = " + g);
+	}
+
+}
