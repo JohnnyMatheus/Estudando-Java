40251e1d929a97107a9b265cfcac78da3b1512b5
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-05-06 02:30:12 -0300

    Create ex4.java

---
+++ estudando_java/src/estrutura_sequencial/ex4.java
@@ -0,0 +1,36 @@
+package estrutura_sequencial;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+/**
+ * Fazer um programa que leia o número de um funcionário, seu número de horas
+ * trabalhadas, o valor que recebe por hora e calcula o salário desse
+ * funcionário. A seguir, mostre o número e o salário do funcionário, com duas
+ * casas decimais
+ * 
+ */
+
+public class ex4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int numFuncionario, horasTrabalhadas;
+		double valorHora, salario;
+
+		System.out.print("Informe o número do funcionário: ");
+		numFuncionario = sc.nextInt();
+		System.out.println("Informe quantas horas trabalhou: ");
+		horasTrabalhadas = sc.nextInt();
+		System.out.println("Informe o valor R$ da hora trabalhada: ");
+		valorHora = sc.nextDouble();
+		salario = (double) horasTrabalhadas * valorHora;
+		System.out.println("Número = " + numFuncionario);
+		Locale.setDefault(Locale.US);
+		System.out.printf("Salário R$ = %.2f", salario);
+
+	}
+
+}
