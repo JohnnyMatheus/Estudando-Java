517e64e1c882e8f52cd46e946adb2502b18eac6a
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-05-07 18:03:03 -0300

    Create ex4.java

---
+++ estudando_java/src/estrutura_condicional/ex4.java
@@ -0,0 +1,31 @@
+package estrutura_condicional;
+
+import java.time.Duration;
+import java.util.Scanner;
+
+/*
+ * Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode 
+começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
+ * */
+public class ex4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Hora Inicial: ");
+		int horaInicial = sc.nextInt();
+		System.out.println("Hora Final");
+		int horaFinal = sc.nextInt();
+		
+		int duracao;
+		if (horaInicial < horaFinal) {
+			duracao = horaFinal - horaInicial;
+		} else {
+			duracao = 24 -horaInicial+horaFinal;
+		}
+
+		System.out.printf("O JOGO DUROU %d HORA(S ", duracao);
+
+	}
+
+}
