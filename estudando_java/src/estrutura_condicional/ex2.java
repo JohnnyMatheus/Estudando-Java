fe465a170981295707286783af20f111b9c0d94a
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-05-07 12:25:11 -0300

    EX2
    Feito exercício 2 estrutura condicional

--- estudando_java/src/estrutura_condicional/ex2.java
+++ estudando_java/src/estrutura_condicional/ex2.java
@@ -1,10 +1,21 @@
 package estrutura_condicional;
-
+import java.util.Scanner;
+/*
+ * Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+ * */
 public class ex2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Informe um número: ");
+		int numero = sc.nextInt();
+		if(numero%2 ==0) {
+			System.out.print("O NÚMERO DIGITADO É PAR");
+		}else {
+			System.out.print("O NÚMERO DIGITADO É IMPAR");
+		}
+		
 	}
 
 }
