0060e33e3d526d8d9a78ab6282f0046f7da1f9c3
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-05-09 01:29:24 -0300

    Create ContagemRegressiva.java

---
+++ estudando_java/src/estrutura_repetitiva_For/ContagemRegressiva.java
@@ -0,0 +1,16 @@
+package estrutura_repetitiva_For;
+
+import java.util.Scanner;
+
+public class ContagemRegressiva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 10;i>=0;i--) {
+			System.out.println(i);
+		}
+		
+		
+	}
+
+}
