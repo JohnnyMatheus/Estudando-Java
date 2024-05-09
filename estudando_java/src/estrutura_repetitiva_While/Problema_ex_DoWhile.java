fc09d8f4a88d42cc9e6e311c437cd255486aca46
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-05-09 17:10:01 -0300

    Update Problema_ex_DoWhile.java

--- estudando_java/src/estrutura_repetitiva_While/Problema_ex_DoWhile.java
+++ estudando_java/src/estrutura_repetitiva_While/Problema_ex_DoWhile.java
@@ -20,7 +20,7 @@ public class Problema_ex_DoWhile {
 			double c = sc.nextDouble();
 			f = 9 * c / 5 + 32;
 			System.out.printf("Equivalente em Fahrenheit: %.1f%n", f);
-			System.out.println("Deseja continuar (s/n)");
+			System.out.println("Deseja continuar (s/n)?");
 			op = sc.next().charAt(0);
 		} while (op != 'n');
 		sc.close();
