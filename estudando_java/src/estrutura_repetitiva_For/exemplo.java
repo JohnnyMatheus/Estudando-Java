b4b9fafc1080cf70879c7496047505fa4fef779c
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-05-09 00:40:11 -0300

    Update exemplo.java

--- estudando_java/src/estrutura_repetitiva_For/exemplo.java
+++ estudando_java/src/estrutura_repetitiva_For/exemplo.java
@@ -1,5 +1,8 @@
 package estrutura_repetitiva_For;
 
+/*Fazer um programa que lê um valor inteiro N e depois N números inteiros.
+ * Ao final, mostra a soma dos N números lidos
+ * */
 import java.util.Scanner;
 
 public class exemplo {
@@ -7,14 +10,14 @@ public class exemplo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Quantos valores deseja ler: ");
 		int n = sc.nextInt();
 		int soma = 0;
 		for (int i = 0; i < n; i++) {
+			System.out.printf("informe o %d° valor: %n",i+1);
 			int x = sc.nextInt();
 			soma+=x;
 		}
-		System.out.println(soma);
-		sc.close();
-
+		System.out.println("A soma dos valores lidos é: "+ soma);
 	}
 }
