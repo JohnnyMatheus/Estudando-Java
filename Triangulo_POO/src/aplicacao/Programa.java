a7bd8d1893b32c770abf6ead951d1eca86d3381d
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-05-10 21:00:26 -0300

    Estudando

--- Triangulo_POO/src/aplicacao/Programa.java
+++ Triangulo_POO/src/aplicacao/Programa.java
@@ -26,20 +26,19 @@ public class Programa {
 		y.b = sc.nextDouble();
 		y.c = sc.nextDouble();
 
-		double p = (x.a + x.b + x.c) / 2.0;
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+		double areaX = x.area();
+		double areaY = y.area();
 
-		p = (y.a + y.b + y.c) / 2.0;
-		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		System.out.printf("Area do Triangulo X %.4f%n", areaX);
 
-		System.out.printf("Triângulo X area: %.4f%n", areaX);
-		System.out.printf("Triângulo Y area: %.4f%n", areaY);
+		System.out.printf("Area do Triangulo Y %.4f%n", areaY);
 
 		if (areaX > areaY) {
-			System.out.printf("Maior area: %.4f", areaX);
+			System.out.printf("Maior area: %.4f%n", areaX);
 		} else {
-			System.out.printf("Maior area: %.4f", areaY);
+			System.out.printf("Maior area: %.4f%n", areaY);
 		}
+
 		sc.close();
 	}
 
