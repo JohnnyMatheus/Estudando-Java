27b67f3d93908c8fc44525fe607e2cc1e0766d13
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-05-11 11:30:54 -0300

    Estudando

--- Retangulo_POO/src/aplicacao/Programa.java
+++ Retangulo_POO/src/aplicacao/Programa.java
@@ -1,6 +1,9 @@
 package aplicacao;
+
 import java.util.Locale;
 import java.util.Scanner;
+import entidade.Retangulo;
+
 /*Fazer um programa para ler os valores da largura e altura
  * de um retângulo. Em seguida, mostrar na tela o valor de 
  * sua área, perímetro e diagonal.
@@ -11,8 +14,17 @@ public class Programa {
 		// TODO Auto-generated method stub
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+
+		Retangulo retangulo = new Retangulo();
+		System.out.print("Informe a largura: ");
+		retangulo.largura = sc.nextDouble();
+		System.out.println("Informe a altura: ");
+		retangulo.altura = sc.nextDouble();
+		
+		System.out.println(retangulo);
 		
 		
+
 	}
 
 }
--- Retangulo_POO/src/entidade/Retangulo.java
+++ Retangulo_POO/src/entidade/Retangulo.java
@@ -3,16 +3,25 @@ package entidade;
 public class Retangulo {
 	public double largura;
 	public double altura;
-	
+
 	public double Area() {
-		return largura *altura;
+		return largura * altura;
 	}
+
 	public double Perimetro() {
-		return Math.pow(largura,2)+Math.pow(altura,2);
+		return (largura * 2) + (altura * 2);
+	}
+
+	public double Diagonal() {
+		return Math.sqrt(Math.pow(largura, 2) + Math.pow(altura, 2));
+	}
+	
+	public String toString() {
+		return "Area: "+String.format("%.2f%n",Area())+
+				"Perimetro: "+String.format("%.2f%n",Perimetro())+
+				"Diagonal: "+String.format("%.2f%n", Diagonal());
 	}
 	
 	
-	
-	
-	
+
 }
