001b2b8c0cda1c821208465dcbb7bccf15b57175
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-05-11 00:10:55 -0300

    ESTUDANDO

---
+++ Estoque_Produto_POO_versao_2_toString/src/entidade/Produto.java
@@ -0,0 +1,30 @@
+package entidade;
+
+public class Produto {
+	public String nome;
+	public double preco;
+	public int quantidade;
+	
+	public double ValorTotalEstoque() {
+		return preco* quantidade;
+	}
+	
+	public void AddProdutos(int quantidade) {
+		this.quantidade += quantidade;
+	}
+	
+	public void RemoverProdutos(int quantidade) {
+		this.quantidade -= quantidade;
+	}
+	
+	public String toString() {
+		return "Nome: "+ nome +", "+
+				"Preço Unitário: R$"+String.format("%.2f", preco)+", "+
+				"Quantidade: "+ quantidade+", "+
+				"Valor Total: R$"+ String.format("%.2f", ValorTotalEstoque());
+	}
+	
+	
+	
+	
+}
