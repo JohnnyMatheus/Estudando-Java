865082abd8cadbae2cfc22888cef7c7a0839485b
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-05-11 00:45:28 -0300

    Estudando

---
+++ Retangulo_POO/src/aplicacao/Programa.java
@@ -0,0 +1,18 @@
+package aplicacao;
+import java.util.Locale;
+import java.util.Scanner;
+/*Fazer um programa para ler os valores da largura e altura
+ * de um retângulo. Em seguida, mostrar na tela o valor de 
+ * sua área, perímetro e diagonal.
+ * */
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+
+}
---
+++ Retangulo_POO/src/entidade/Retangulo.java
@@ -0,0 +1,18 @@
+package entidade;
+
+public class Retangulo {
+	public double largura;
+	public double altura;
+	
+	public double Area() {
+		return largura *altura;
+	}
+	public double Perimetro() {
+		return Math.pow(largura,2)+Math.pow(altura,2);
+	}
+	
+	
+	
+	
+	
+}
