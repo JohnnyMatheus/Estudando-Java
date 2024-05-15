81f5a66a16bd42ea1eee7e13865fc21fe6f7f413
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-05-15 17:16:24 -0300

    Update Ex3_vetor_alturas.java

--- Exercicios_Fixacao_Vetores_SemClasse/src/Ex3_vetor_alturas.java
+++ Exercicios_Fixacao_Vetores_SemClasse/src/Ex3_vetor_alturas.java
@@ -1,6 +1,6 @@
 import java.util.Locale;
 import java.util.Scanner;
-
+import java.util.Scanner;
 /*Fazer um programa para ler nome, idade e altura de N pessoas, conforme exemplo. Depois, mostrar na 
  *tela a altura média das pessoas, e mostrar também a porcentagem de pessoas com menos de 16 anos, 
  *bem como os nomes dessas pessoas caso houver. 
@@ -10,7 +10,18 @@ public class Ex3_vetor_alturas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Informe quantos dados deseja ler: ");
+		int n = sc.nextInt();
+		String[] nome = new String[n];
+		int[] idade = new int[n];
+		double[] altura = new double[n];
+
+		for (int i = 0; i < n; i++) {
+
+		}
+
 	}
 
 }
