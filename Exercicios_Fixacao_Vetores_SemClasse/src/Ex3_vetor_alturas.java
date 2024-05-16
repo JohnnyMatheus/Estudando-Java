eb05a17e8b46ca82082e7dd4d045034bb2da901f
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-05-16 01:19:54 -0300

    Update Ex3_vetor_alturas.java

--- Exercicios_Fixacao_Vetores_SemClasse/src/Ex3_vetor_alturas.java
+++ Exercicios_Fixacao_Vetores_SemClasse/src/Ex3_vetor_alturas.java
@@ -1,6 +1,5 @@
 import java.util.Locale;
 import java.util.Scanner;
-import java.util.Scanner;
 /*Fazer um programa para ler nome, idade e altura de N pessoas, conforme exemplo. Depois, mostrar na 
  *tela a altura média das pessoas, e mostrar também a porcentagem de pessoas com menos de 16 anos, 
  *bem como os nomes dessas pessoas caso houver. 
@@ -19,15 +18,36 @@ public class Ex3_vetor_alturas {
 		double[] altura = new double[n];
 
 		for (int i = 0; i < n; i++) {
-			System.out.printf("%d°Nome: ", i + 1);
+			System.out.printf("Dados da %d° pessoa:%n", i + 1);
+			System.out.print("Nome: ");
 			sc.nextLine();
 			nome[i] = sc.nextLine();
-			System.out.printf("%d°Idade: ", i + 1);
+			System.out.print("Idade: ");
 			idade[i] = sc.nextInt();
-			System.out.printf("%d°Altura: ", i + 1);
+			System.out.print("Altura: ");
 			altura[i] = sc.nextDouble();
 		}
 
+		int menores = 0;
+		double soma = 0.0;
+		for (int i = 0; i < n; i++) {
+			soma += altura[i];
+			if (idade[i] > 16) {
+				menores++;
+			}
+		}
+		double alturaMedia = soma / altura.length;
+		double percentual = ((double) menores / n) * 100;
+
+		System.out.printf("Altura média: %.2f%n", alturaMedia);
+		System.out.printf("Pessoas com menos de 16 anos: %.1f%%%n", percentual);
+		for (int i = 0; i < n; i++) {
+			if (idade[i] < 16) {
+				System.out.println(nome[i]);
+			}
+		}
+
+		sc.close();
 	}
 
 }
