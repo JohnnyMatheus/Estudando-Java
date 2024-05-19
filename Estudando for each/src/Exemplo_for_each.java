2f802ab520225a71db66899760cce6307bbe0f5e
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-05-19 02:01:26 -0300

    estudando

---
+++ Estudando for each/src/Exemplo_for_each.java
@@ -0,0 +1,14 @@
+public class Exemplo_for_each {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nomes: ");
+		String[] vetor = new String[] { "Johnny", "Matheus", "teste", "Computação" };
+		//PARA CADA OBJETO NOME CONTIDO NO VETOR FAÇA
+		for (String nome : vetor) {
+			System.out.println(nome);
+		}
+
+	}
+
+}
