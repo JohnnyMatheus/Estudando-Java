3a34236b0561f1c480292fe3693102f739c44081
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-05-29 16:59:21 -0300

    Update Programa.java

--- Exercicio proposto Matriz/src/aplicacao/Programa.java
+++ Exercicio proposto Matriz/src/aplicacao/Programa.java
@@ -1,10 +1,18 @@
 package aplicacao;
-
+import java.util.Scanner;
 public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		int[][] matriz = new int[m][n];
+		
+		for(int i =0;i<matriz.length;i++) {
+			
+		}
+		
 	}
 
 }
